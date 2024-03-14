ea6e7319427971122786cba432e66dbba110711f
Wosmos <m.wasifmalik17@gmail.com>
2024-03-14 14:19:25 +0500

    done -2

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,11 @@ import SectionDivider from '@/components/section-divider';
 import Skills from '@/components/skills';
 import Blogs from '@/components/blogs';
 import TechStack from '@/components/tech-stack';
-import { Head } from 'next/document';
+
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Wosmo | Front-end Developer Portfolio</title>
-        <meta
-          name='description'
-          content='Wosmo (Muhammad Wasif Malik) is a skilled Front-end Developer with 2+ years of experience in building modern web applications using cutting-edge technologies.'
-        />
-        <meta
-          name='keywords'
-          content='Front-end Developer, Web Developer, React, JavaScript, HTML, CSS, Portfolio, Wosmo, Muhammad Wasif Malik'
-        />
-        <meta name='author' content='Muhammad Wasif Malik' />
-        <meta name='robots' content='index, follow' />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://wosmo.com'/>
-        <meta property='og:title' content='Wosmo | Front-End Dev'/>
-        <link rel='canonical' href='https://wosmo.com' />
-        {/* Other head tags */}
-      </Head>
+      
       <main className='flex flex-col items-center px-4'>
         <Intro />
         <SectionDivider />
